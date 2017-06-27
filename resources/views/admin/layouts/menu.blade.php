67d84d0081be92fcd781af3d7985f00ee97ab9b9
haikal <muhamad.haekal@go-jek.com>
2017-06-27 23:10:39 +0700

    make menu active when in its link

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -1,11 +1,11 @@
 <ul class="sidebar-menu tree" data-widget="tree">
     <li class="header">MAIN NAVIGATION</li>
-    <li class="treeview">
+    <li class="treeview {{ request()->is('*admin') ? 'active':'' }}">
       <a href="{{ route('admin.dashboard') }}">
         <i class="fa fa-dashboard"></i> <span>Dashboard</span>
       </a>
     </li>
-    <li class="treeview">
+    <li class="treeview {{ request()->is('*categories*') ? 'active':'' }}">
       <a href="#">
         <i class="fa fa-book"></i> <span>Categories</span>
         <span class="pull-right-container">
@@ -17,7 +17,7 @@
         <li><a href="{{ route('admin.categories.create') }}"><i class="fa fa-plus"></i> create</a></li>
       </ul>
     </li>
-    <li class="treeview">
+    <li class="treeview {{ request()->is('*destinations*') ? 'active':'' }}">
       <a href="#">
         <i class="fa fa-suitcase"></i> <span>Destinations</span>
         <span class="pull-right-container">
@@ -29,7 +29,7 @@
         <li><a href="{{ route('admin.destinations.create') }}"><i class="fa fa-plus"></i> create</a></li>
       </ul>
     </li>
-    <li class="treeview">
+    <li class="treeview {{ request()->is('*users*') ? 'active':'' }}">
       <a href="#">
         <i class="fa fa-users"></i> <span>Users</span>
         <span class="pull-right-container">
@@ -40,7 +40,7 @@
         <li><a href="{{ route('admin.users.index') }}"><i class="fa fa-list"></i> index</a></li>
       </ul>
     </li>
-    <li class="treeview">
+    <li class="treeview {{ request()->is('*purchases*') ? 'active':'' }}">
       <a href="#">
         <i class="fa fa-users"></i> <span>Purchases</span>
         <span class="pull-right-container">
